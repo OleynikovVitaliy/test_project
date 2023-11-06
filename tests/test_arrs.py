67114051f80072533805231e21dd49330f9c54e9
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2023-11-06 18:14:21 +0500

    add_new_tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -3,9 +3,14 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
-
+    assert arrs.get([1, 2, 3, 4, 5], 10, default="Not found") == "Not found"
+    assert arrs.get([1, 2, 3, 4, 5], 3) != 5
+    assert arrs.get([], 0, default="Empty") == "Empty"
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5]) != []
+    assert arrs.my_slice([1, 2, 3, 4, 5], end=3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5], start=-3, end=-1) == [3, 4]
